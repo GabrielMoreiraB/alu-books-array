e1f976964e3ab4cdbf32c84c68224bcab0befd57
Gabriel Barbosa Moreira <109036024+GabrielMoreiraB@users.noreply.github.com>
2023-02-27 17:57:25 -0300

    .

--- app/main.js
+++ app/main.js
@@ -1,10 +1,32 @@
 let livros = [];
 const endPointAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json';
 
+const elementoParaIncerirLivros = document.getElementById('livros');
+
 getBuscaLivrosAPI();
 
 async function getBuscaLivrosAPI(){
     const resp = await fetch(endPointAPI);
     livros = await resp.json(); 
     console.table(livros);
+    exibirLivros(livros)
+}
+
+
+function exibirLivros(listaDeLivros) {
+    listaDeLivros.forEach(function (livro) {
+        elementoParaIncerirLivros.innerHTML += `
+        <div class="livro">
+      <img class="livro__imagens" src="${livro.imagem}"
+        alt="${livro.alt}" />
+      <h2 class="livro__titulo">
+        ${livro.titulo}
+      </h2>
+      <p class="livro__descricao">${livro.autor}</p>
+      <p class="livro__preco" id="preco">R$${(livro.preco).toFixed(2)}</p>
+      <div class="tags">
+        <span class="tag">${livro.categoria}</span>
+      </div>
+    </div>`
+    });
 }
